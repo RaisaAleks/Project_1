3dbfd9c607d98d8ce31180d8ba4241fac9fa12d4
RaisaAleks <128984057+RaisaAleks@users.noreply.github.com>
2024-06-26 22:10:48 +0400

    Add files via upload

---
+++ project1_6.sql
@@ -0,0 +1,8 @@
+SELECT 
+    quantity,
+    product_category,
+	gender,
+	COUNT(customer_id)
+FROM project_1
+GROUP BY quantity, product_category,gender
+ORDER BY quantity DESC;          
---
+++ project1_7.sql
@@ -0,0 +1,8 @@
+SELECT 
+    quantity,
+    product_category,
+    SUM(price_per_unit) AS sum_price_per_unit,
+    AVG(price_per_unit) AS avg_price_per_unit
+FROM project_1
+GROUP BY quantity, product_category
+ORDER BY quantity DESC;  
